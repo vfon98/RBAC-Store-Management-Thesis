607fcf2f22e9868e18fe0ab55264c0c46046b357
vfon98 <vfon98@gmail.com>
2020-10-10 12:42:46 +0700

    Feat: added Hot Module Reload (hmr) to project

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -1,3 +1,4 @@
+import { hmrBootstrap } from './hmr';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -8,5 +9,15 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch((err) => console.log(err));
+}
